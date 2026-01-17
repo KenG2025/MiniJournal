c6f77733970a62f9778961249237ae2ec13d985b
KenG2025 <kenny.gonzalez@sdgku.edu>
2026-01-16 22:53:50 -0500

    MDI-108 assignment3.2

--- MiniJournal/MiniJournalApp.swift
+++ MiniJournal/MiniJournalApp.swift
@@ -5,28 +5,19 @@
 //  Created by Ken Gonzalez on 1/10/26.
 //
 
+
 import SwiftUI
 import SwiftData
 
 @main
-struct MiniJournalApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
+struct Class04App: App {
     var body: some Scene {
         WindowGroup {
-            EntryListView()
+            NavigationStack {
+                EntryListView()
+            }
         }
-        .modelContainer(sharedModelContainer)
+        .modelContainer(for: JournalEntry.self)
     }
 }
+
